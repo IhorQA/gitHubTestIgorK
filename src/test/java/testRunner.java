7bb8cf536d5f591aed9128886d103290866b5c69
Julia <ihor_k@ukr.net>
2016-03-03 20:37:46 +0200

    Add Before test annotation and metod

--- src/test/java/testRunner.java
+++ src/test/java/testRunner.java
@@ -1,12 +1,14 @@
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 /**
  * Created by Igor K on 03.03.2016.
  */
 public class testRunner {
+    @BeforeTest
+    public void ThisIsBeforeTest(){
+        System.out.println("This Is OUR TEST CASE (Before Test)");
+    }
+
     @BeforeMethod
     public void ThisBeforeTestMethod(){
         System.out.println("This Before Test Method");
